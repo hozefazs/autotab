673835ffd536200c9a589b30fdcaeea6eb6ee7e3
Hozefa Sadriwala <hozefazs@gmail.com>
2021-11-29 17:48:27 +0400

    model load and display

--- autotab/TabPrediction.py
+++ autotab/TabPrediction.py
@@ -1,4 +1,5 @@
 import autotab.TabCNN as TabCNN
+import pandas as pd
 """Script to predict the tabs from a model"""
 """
 #### PSEUDOCODE
@@ -18,7 +19,7 @@ import autotab.TabCNN as TabCNN
 """
 
 
-def full_tab(y_pred):
+def make_empty_tab():
     tab_dict = {
         'E': [],
         'A': [],
@@ -29,35 +30,66 @@ def full_tab(y_pred):
     }
     tablature = pd.DataFrame.from_dict(tab_dict)
     tablature = tablature.T
-    for frame_idx in range(0, len(y_pred)):
-        this_frame = pd.DataFrame(y_pred[frame_idx])
-        tablature[str(frame_idx)] = this_frame.idxmax(axis='columns')[0]
     return tablature
 
 
-##################################
-########### LOAD MODEL & PREDICT ###########
-##################################
-tabcnn = TabCNN()
-# tabcnn.clear_previously_created_nodes()
+def make_full_tab(labels, num_frames=99):
+    tablature = make_empty_tab()
+    # loop over all frames to add for all frame (lets test for num_frames frames)
+    for frame_idx in range(0, num_frames):
+        frame = pd.DataFrame(
+            labels[frame_idx])  # loading frame frame_idx into a dataframe
+        # now set (Frame, 0 Fret, where value < 0.9) set as 0
+        frame[0].replace(to_replace=list(frame[0][frame[0] < 0.9]),
+                         value=0,
+                         inplace=True)
+        #now use idmax to check best fret for every string
+        fret = list(frame.idxmax(axis='columns'))
+        #add fret to tablature
+        tablature[frame_idx] = fret
+    return tablature
 
-print("logging model...")
-tabcnn.build_model()
-tabcnn.log_model()
 
-# Note : our dataset has 6 set of files starting from 00 upto 05
-for fold in range(6):  # The 'fold' is used as the validation set
-    print("\nfold " + str(fold))  #printing the fold number
-    tabcnn.partition_data(
-        fold)  # partitionig of data, with the fold used as validation
-    print("building model...")
-    tabcnn.build_model()  #building the model to do the training
-    print("training...")
-    tabcnn.train()
-    tabcnn.save_weights()
-    print("testing...")
-    tabcnn.test()
-    tabcnn.save_predictions()
-    print("evaluation...")
-    tabcnn.evaluate()
-print("saving results...")
+def make_squeezed_tab(tablature, n=9):
+    """Make displayable squeezed tablature
+        Args:
+        tablature : the full tablature made from labels
+        n : the frame squeeze window. Currently using same size as con-win
+    """
+    #now do it for all n frames
+    squeezed_Tab = make_empty_tab()
+    for batch_idx in range(0, tablature.shape[1], n):
+        # for every frame_batch
+        frame_batch = tablature.loc[:, batch_idx:batch_idx + n - 1]
+        squeezed_Tab[batch_idx] = frame_batch.mode(axis='columns')[0]
+    return squeezed_Tab.astype(int)
+
+
+if __name__ == "__main__":
+    ##################################
+    ########### LOAD MODEL & PREDICT ###########
+    ##################################
+    tabcnn = TabCNN()
+    # tabcnn.clear_previously_created_nodes()
+
+    print("logging model...")
+    tabcnn.build_model()
+
+    tabcnn.log_model()
+
+    # Note : our dataset has 6 set of files starting from 00 upto 05
+    for fold in range(6):  # The 'fold' is used as the validation set
+        print("\nfold " + str(fold))  #printing the fold number
+        tabcnn.partition_data(
+            fold)  # partitionig of data, with the fold used as validation
+        print("building model...")
+        tabcnn.build_model()  #building the model to do the training
+        print("training...")
+        tabcnn.train()
+        tabcnn.save_weights()
+        print("testing...")
+        tabcnn.test()
+        tabcnn.save_predictions()
+        print("evaluation...")
+        tabcnn.evaluate()
+    print("saving results...")
