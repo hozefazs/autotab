47551ee81b68e466631a16f361d77295e1af376a
Ruslan Zitser <ruslan.sizer@gmx.de>
2021-12-12 16:09:09 +0100

    working download from bucket into docker container

--- api/api_app.py
+++ api/api_app.py
@@ -4,7 +4,8 @@ import os
 from autotab.TabDataReprGen import TabDataReprGen
 import autotab.TabPrediction as tp
 from autotab import param
-import glob
+import time
+
 
 app = FastAPI()
 
@@ -21,15 +22,6 @@ model.load_weights('./h5-model/full_val0_75acc_weights.h5')
 genrep = TabDataReprGen()
 
 
-# Testing out of of the server in docker
-print(os.getcwd())
-
-#########################
-def get_gcloud_file(filename):
-#gcloud storage
-    gcloud_path = f'gsutil cp -r gs://{param.BUCKET_NAME}/{filename} .'
-    print(gcloud_path)
-    os.popen(gcloud_path)
 
 # get filename from User Interface! 
 
@@ -44,12 +36,7 @@ def get_gcloud_file(filename):
 # root endpoint 
 @app.get('/ergonomic_simple')
 def ergonomic_simple(uploaded_file):
-    print(uploaded_file)
-    if uploaded_file is not None:
-        get_gcloud_file(uploaded_file)
-        x_new = genrep.load_rep_from_raw_file(uploaded_file)
-        y_pred = model.predict(x_new)
-    
+    y_pred = load_predict(uploaded_file)   
     all_frames_tab = tp.make_smart_tab(y_pred, len(y_pred))
     simple_tabs = tp.make_squeezed_tab(all_frames_tab)
     simple_text = tp.web_tabs(simple_tabs) 
@@ -59,12 +46,7 @@ def ergonomic_simple(uploaded_file):
 
 @app.get('/ergonomic_rhythm')
 def ergonomic_rhythm(uploaded_file):
-    print(uploaded_file)
-    if uploaded_file != None:      
-        get_gcloud_file(uploaded_file)
-        x_new = genrep.load_rep_from_raw_file(uploaded_file)
-        y_pred = model.predict(x_new)
-    
+    y_pred = load_predict(uploaded_file)
     all_frames_tab = tp.make_smart_tab(y_pred, len(y_pred))
     rythm_tabs = tp.make_dynamic_tab(all_frames_tab)
     rythm_text = tp.web_tabs(rythm_tabs)
@@ -74,15 +56,32 @@ def ergonomic_rhythm(uploaded_file):
 
 @app.get('/all_frames')
 def all_frames(uploaded_file):
-    print(uploaded_file)
-    if uploaded_file is not None:     
-        get_gcloud_file(uploaded_file)   
-        x_new = genrep.load_rep_from_raw_file(uploaded_file)
-        y_pred = model.predict(x_new)
-    
+    y_pred = load_predict(uploaded_file)
     all_frames_tab = tp.make_smart_tab(y_pred, len(y_pred))
     all_frames_text = tp.web_tabs(all_frames_tab)
     print(all_frames_text)
     
     ###################################################
     return {'simple_text': all_frames_text}
+
+def load_predict(uploaded_file):
+    print(uploaded_file)
+    if uploaded_file is not None:     
+        get_gcloud_file(uploaded_file)   
+        x_new = genrep.load_rep_from_raw_file(uploaded_file)
+        y_pred = model.predict(x_new)
+    return y_pred
+
+# Testing out of of the server in docker
+print(os.getcwd())
+
+#########################
+def get_gcloud_file(filename):
+    
+#gcloud storage
+    try: 
+        gcloud_path = f'gsutil cp -n gs://{param.BUCKET_NAME}/{filename} .'
+    except:
+        time.sleep(5)
+    print(gcloud_path)
+    os.popen(gcloud_path)
